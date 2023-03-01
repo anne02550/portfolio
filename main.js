55c82e6a3f5b3e5c748e67934f7f775889657b81
Tam Pham <anne02550@gmail.com>
2023-03-01 22:58:00 +0000

    Squashing everything

---
+++ main.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3100;
+
+app.use('/public', express.static('public'));
+app.use('/css', express.static('public/css'));
+app.use('/js', express.static('public/js'));
+app.use('/img', express.static('public/img'));
+app.use('/fonts', express.static('public/fonts'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/html/index.html');
+});
+
+app.get('/portfolio', (req, res) => {
+  res.sendFile(__dirname + '/public/html/portfolio.html');
+});
+
+app.get('/resume', (req, res) => {
+  res.sendFile(__dirname + '/public/html/resume.html');
+});
+
+app.listen(port, () => {
+  console.log(`portfolio listening at http://localhost:${port}`)
+});
